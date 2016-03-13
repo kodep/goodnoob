1779499b38eb19eb56fbf9a80199b036d0fa85f9
Новый пользователь <tsipa88@gmail.com>
2016-03-13 23:18:09 +0200

    load more button for subcategories

--- app/controllers/sub_categories_controller.rb
+++ app/controllers/sub_categories_controller.rb
@@ -1,12 +1,18 @@
 class SubCategoriesController < ApplicationController
   def show
     @sub_category = SubCategory.find(params[:id])
-    @products = @sub_category.products.filter(params[:filters])
-                                      .companies(params[:companies])
-                                      .year_from(params[:year_from])
-                                      .year_to(params[:year_to])
-                                      .price_from(params[:price_from])
-                                      .price_to(params[:price_to])
+
+    @products = @sub_category.products
+                             .filter(params[:filters])
+                             .companies(params[:companies])
+                             .year_from(params[:year_from])
+                             .year_to(params[:year_to])
+                             .price_from(params[:price_from])
+                             .price_to(params[:price_to])
+                             .page(params[:page])
+                             .per(16)
+                             .order(created_at: :asc)
+
     @filters = @sub_category.filters
 
     respond_to do |format|
