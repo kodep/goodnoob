9ff36f073acca3df8c84c25e53b8fe299199a652
Oleg Sulyanov <oleg@sulyanov.com>
2016-04-04 19:16:23 +0700

    Created guides controller

---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,17 @@
+class GuidesController < ApplicationController
+  before_action :set_sub_category, only: [:show]
+  before_action :set_guide, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_sub_category
+
+  end
+
+  def set_guide
+
+  end
+end
