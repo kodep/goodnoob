dd324f12a9d2c053f0fbc593830cdfc117a474eb
Dmitry Y <hds101@gmail.com>
2017-01-12 14:33:11 +0300

    Review page fixes. refs #25

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -437,8 +437,8 @@ $(document).on('turbolinks:load', function() {
     // GreenTree
     $('.more-less-text').readmore({
         collapsedHeight: 45,
-        moreLink: '<a>(Read more)</a>',
-        lessLink: '<a>(Read less)</a>'
+        moreLink: '<a href="javascript:;">(Read more)</a>',
+        lessLink: '<a href="javascript:;">(Read less)</a>'
     });
 
     $(document).ready(function(){
