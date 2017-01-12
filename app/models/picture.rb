47117536549b06db17991a42165844df59c49aa5
Dmitry Y <hds101@gmail.com>
2017-01-12 11:46:42 +0300

    Paperclip avatar thumbnail. refs #30

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ActiveRecord::Base
 
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, styles: { :subcatcover => '1500x844#', thumbnail: '250x190>' }
+  has_attached_file :image, styles: { :subcatcover => '1500x844#', thumbnail: '250x190>', avatar: '165x165#' }
   validates_attachment :image, presence: true, content_type: { content_type: /\Aimage\/.*\Z/ }
 
   delegate :url, to: :image
