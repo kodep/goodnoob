659ce96121185a58b8f7b8447477e4688e9be564
Dmitry A <mr.ads@yandex.ru>
2017-01-17 09:48:19 +0300

    added paperclip optimizer && medium size of picture. refs #104, #16

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,8 +3,16 @@ class Picture < ActiveRecord::Base
 
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, styles: { :subcatcover => '1500x844#', thumbnail: '250x190>', avatar: '165x165#' }
-  validates_attachment :image, presence: true, content_type: { content_type: /\Aimage\/.*\Z/ }
+  has_attached_file :image,
+                    processors: [:thumbnail, :paperclip_optimizer],
+                    styles: { subcatcover: '1500x844#',
+                              thumbnail: '250x190>',
+                              medium: '470x300#',
+                              avatar: '165x165#' }
+
+  validates_attachment :image,
+                       presence: true,
+                       content_type: { content_type: /\Aimage\/.*\Z/ }
 
   delegate :url, to: :image
 end
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module GoodNoob
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
-
+    config.assets.image_optim = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
