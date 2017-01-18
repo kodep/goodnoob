0ef806abe25bf5cd6308cdad4c9870c55cf66582
Dmitry Y <hds101@gmail.com>
2017-01-18 15:22:42 +0300

    Add FAQ. refs #122

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118115404) do
+ActiveRecord::Schema.define(version: 20170118120937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,13 @@ ActiveRecord::Schema.define(version: 20170118115404) do
   add_index "guides", ["sub_category_id"], name: "index_guides_on_sub_category_id", using: :btree
 
   create_table "info_privacies", force: :cascade do |t|
+    t.string   "header"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "info_questions", force: :cascade do |t|
     t.string   "header"
     t.text     "description"
     t.datetime "created_at",  null: false
