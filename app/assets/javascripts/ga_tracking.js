f65f1084481c87597bd5edcc1d52d06765f73657
bbwstar <sashaverify@gmail.com>
2017-03-13 13:22:07 +0200

    Add files via upload

---
+++ app/assets/javascripts/ga_tracking.js
@@ -0,0 +1,30 @@
+function send_track_signup() {
+
+
+	ga('send', 'event', 'signup', 'signup', 'Fall Campaign');
+}
+
+function handleOutboundLinkClicks(event) {
+  ga('send', 'event', {
+    eventCategory: 'Outbound Link',
+    eventAction: 'click',
+    eventLabel: event.target.href
+  });
+}
+
+
+function send_track_signup_complete() {
+	ga('send', 'clicksignupcomplete');
+}
+
+function send_track_signup() {
+	ga('send', 'clicksignup');
+}
+
+function send_track_signup() {
+	ga('send', 'clicksignup');
+}
+
+function send_track_signup() {
+	ga('send', 'clicksignup');
+}
