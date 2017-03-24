9eaaca74dd712deebd147a63c5433efb64c13cbb
Duc Giang <giangnguyen1089@gmail.com>
2017-03-24 22:22:50 +0700

    fix type filter search

--- app/models/product.rb
+++ app/models/product.rb
@@ -119,7 +119,7 @@ class Product < ActiveRecord::Base
   def self.base_search(search, sub_categories, companies)
     prods = all
     if search || sub_categories.is_a?(Array) || companies.is_a?(Array)
-      prods = prods.where('lower(products.name) LIKE lower(:search) or lower(products.description) LIKE lower(:search)', search: "%#{search}%") if search
+      prods = prods.where('lower(products.name) LIKE lower(:search) or lower(products.description_en) LIKE lower(:search)', search: "%#{search}%") if search
       prods = prods.where(sub_category_id: sub_categories) if sub_categories.is_a?(Array)
       prods = prods.where(company_id: companies) if companies.is_a?(Array)
       prods
