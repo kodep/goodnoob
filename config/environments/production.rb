e6bb7825cb8be2d2fda904f5a8448f2998da1f1d
Duc Giang <giangnguyen1089@gmail.com>
2017-05-25 19:38:06 +0700

    migrate from Postmark to Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '465',
+    authentication: :plain,
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
+    enable_starttls_auto: true,
+    ssl: true
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {
+    host: Rails.application.secrets.domain_name
+  }
+  config.action_mailer.default_options = {
+    from: "no-reply@goodnoob.com"
+  }
 end
